7e00fcfab2fafb10d9eca937cc70319f143bebb3
Arijeet De <arijeetde@gmail.com>
2023-06-07 12:12:37 +0530

    bug fixing in nav bar

--- js/toggle_nav.js
+++ js/toggle_nav.js
@@ -2,6 +2,14 @@ const toggleBtn = document.querySelector(".nav-toggle");
 const toggleBtnIcon = document.querySelector(".nav-toggle i");
 const dropDownMenu = document.querySelector(".dropdown-menu");
 
+window.onload = function () {
+    const isOpen = dropDownMenu.classList.contains('open');
+    if (isOpen) {
+        dropDownMenu.classList.remove('open');
+        toggleBtnIcon.classList = 'fa-solid fa-bars';
+    }
+};
+
 toggleBtn.addEventListener('click', function () {
     dropDownMenu.classList.toggle('open');
 
