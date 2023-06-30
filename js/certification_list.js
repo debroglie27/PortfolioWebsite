4779ab0a4cd0a1a1dcf657ed0d5cba5986ce429d
Arijeet De <arijeetde@gmail.com>
2023-06-30 19:26:34 +0530

    separate certification list & load certification

---
+++ js/certification_list.js
@@ -0,0 +1,76 @@
+const certificationList = [
+    {
+        "id": 1,
+        "title": "Neural Networks and Deep Learning",
+        "institute": "Coursera",
+        "link": "https://drive.google.com/file/d/1llS-1XlbSv-pOjowDwkzrSelj6RGgmhE/view?usp=sharing"
+    },
+    {
+        "id": 2,
+        "title": "Improving Deep Neural Networks: Hyperparameter tuning, Regularization and Optimization",
+        "institute": "Coursera",
+        "link": "https://drive.google.com/file/d/1CSmSqhdDw3AcAEQ20kFh7HxqCCcbe_L_/view?usp=sharing"
+    },
+    {
+        "id": 3,
+        "title": "Structuring Machine Learning Projects",
+        "institute": "Coursera",
+        "link": "https://drive.google.com/file/d/1vV3WZzFVf8zI48sffVFMsIw7acoeo38L/view?usp=sharing"
+    },
+    {
+        "id": 4,
+        "title": "The Analytics Edge",
+        "institute": "edX MIT",
+        "link": "https://drive.google.com/file/d/1gVuRP6HICkQ4b9MVqkS-oSglb9mZ_4Uw/view?usp=sharing"
+    },
+    {
+        "id": 5,
+        "title": "Introduction to Machine Learning",
+        "institute": "Kaggle",
+        "link": "https://drive.google.com/file/d/1L5Jn5Y7MJiEJwVs7SMGkgMYaPLbYH3ua/view?usp=sharing"
+    },
+    {
+        "id": 6,
+        "title": "Intermediate Machine Learning",
+        "institute": "Kaggle",
+        "link": "https://drive.google.com/file/d/1d4zWG8HpVDgFDJvIiELOERDXZyF9HJJV/view?usp=sharing"
+    },
+    {
+        "id": 7,
+        "title": "Data Visualization",
+        "institute": "Kaggle",
+        "link": "https://drive.google.com/file/d/1w26H-CAg8b56y0v92QFFVpW3OlZATPzH/view?usp=sharing"
+    },
+    {
+        "id": 8,
+        "title": "Feature Engineering",
+        "institute": "Kaggle",
+        "link": "https://drive.google.com/file/d/1J7SBpLp4UmPWXlxP4PMccx_Pu_0XGzbT/view?usp=sharing"
+    },
+    {
+        "id": 9,
+        "title": "Course on Pandas",
+        "institute": "Kaggle",
+        "link": "https://drive.google.com/file/d/1d6Xyuw-6am-UdQB1wJqxhHw7N9ubKJOn/view?usp=sharing"
+    },
+    {
+        "id": 10,
+        "title": "Course on Python",
+        "institute": "Kaggle",
+        "link": "https://drive.google.com/file/d/1JZBn24nWAbjDaepNXnvu06HJE8lCfjlz/view?usp=sharing"
+    },
+    {
+        "id": 11,
+        "title": "Artificial Intelligence",
+        "institute": "SkillUp",
+        "link": "https://drive.google.com/file/d/1QeRlWb-DHnZoHPGjNvvsXs0sLgfhK9pq/view?usp=sharing"
+    },
+    {
+        "id": 12,
+        "title": "Course on Microsoft Excel",
+        "institute": "Udemy",
+        "link": "https://drive.google.com/file/d/1bUpoQiwUe8rek7PZuFuMnTwI3ozQgHsx/view?usp=sharing"
+    }
+];
+
+export {certificationList};
