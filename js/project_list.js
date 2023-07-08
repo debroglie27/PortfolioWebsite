0ced1a152ca32dce6aec5458ca3d9d483238eaef
Arijeet De <arijeetde@gmail.com>
2023-07-08 13:28:57 +0530

    writing summary for the projects

--- js/project_list.js
+++ js/project_list.js
@@ -2,63 +2,49 @@ const projectList = [
     {
         title: "Portfolio Website",
         category: "web",
-        summary: "This project is a web version of my resume. The website which you are viewing right now is itself the project. This was made using vanilla HTML, CSS and Javascript.",
+        summary: "A web version of my Resume. This was made using vanilla HTML, CSS and Javascript.",
         link: "https://github.com/debroglie27/PortfolioWebsite",
     },
     {
         title: "PDF Merger App",
         category: "software",
-        summary: "Lorem ipsum, dolor sit amet conse ctetur adipi sicing elit. \
-        Laboriosam dolores qui aperiam impedit? Recu sandae doloribus, \
-        rerum ratione magnam nam quod.",
+        summary: "Created a GUI using Python, Tkinter to merge pdf files in noncontiguous manner.",
         link: "https://github.com/debroglie27/PDFMergerApp",
     },
     {
         title: "Currency Converter App",
         category: "software",
-        summary: "Lorem ipsum, dolor sit amet conse ctetur adipi sicing elit. \
-        Laboriosam dolores qui aperiam impedit? Recu sandae doloribus, \
-        rerum ratione magnam nam quod.",
+        summary: "Created a GUI using Python, Tkinter to convert one form of currency to another with the help of an API.",
         link: "https://github.com/debroglie27/CurrencyConverterApp",
     },
     {
         title: "Ludo Game",
         category: "game",
-        summary: "Lorem ipsum, dolor sit amet conse ctetur adipi sicing elit. \
-        Laboriosam dolores qui aperiam impedit? Recu sandae doloribus, \
-        rerum ratione magnam nam quod.",
+        summary: "Created a well known board game using Python, Pygame.",
         link: "https://github.com/debroglie27/LudoGame",
     },
     {
         title: "Age Calculator App",
         category: "web",
-        summary: "Lorem ipsum, dolor sit amet conse ctetur adipi sicing elit. \
-        Laboriosam dolores qui aperiam impedit? Recu sandae doloribus, \
-        rerum ratione magnam nam quod.",
+        summary: "Created a web page, displaying age based on date of birth, with simple HTML, CSS and JS.",
         link: "https://github.com/debroglie27/AgeCalculatorApp",
     },
     {
         title: "QR Code Component",
         category: "web",
-        summary: "Lorem ipsum, dolor sit amet conse ctetur adipi sicing elit. \
-        Laboriosam dolores qui aperiam impedit? Recu sandae doloribus, \
-        rerum ratione magnam nam quod.",
+        summary: "Designed a web page based on a FrontEnd Mentor challenge.",
         link: "https://github.com/debroglie27/QRCodeComponent",
     },
     {
         title: "Single Price Grid Component",
         category: "web",
-        summary: "Lorem ipsum, dolor sit amet conse ctetur adipi sicing elit. \
-        Laboriosam dolores qui aperiam impedit? Recu sandae doloribus, \
-        rerum ratione magnam nam quod.",
+        summary: "Designed a web page based on a FrontEnd Mentor challenge.",
         link: "https://github.com/debroglie27/SinglePriceGridComponent",
     },
     {
         title: "Testimonials Grid Section",
         category: "web",
-        summary: "Lorem ipsum, dolor sit amet conse ctetur adipi sicing elit. \
-        Laboriosam dolores qui aperiam impedit? Recu sandae doloribus, \
-        rerum ratione magnam nam quod.",
+        summary: "Designed a web page based on a FrontEnd Mentor challenge.",
         link: "https://github.com/debroglie27/TestimonialsGridSection",
     },
 ];
